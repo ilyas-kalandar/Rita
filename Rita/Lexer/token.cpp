5945b42803628d4d1a7923b091b914647583eb14
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-06-16 16:21:47 +0500

    Added constructors and GetLiteral implementations

--- Rita/Lexer/token.cpp
+++ Rita/Lexer/token.cpp
@@ -2,5 +2,18 @@
 
 namespace Lexer
 {
+    Token::Token(TokenType type, std::string& literal) : Literal(std::move(literal))
+    {
+        this->Type = type;
+    }
 
+    Token::Token(TokenType type)
+    {
+        this->Type = type;
+    }
+
+    const std::string& Token::GetLiteral()
+    {
+        return this->Literal;
+    }
 }
