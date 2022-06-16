509506ffb3aef9be0976b8ea9647c023d2b95a2b
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-06-16 16:23:30 +0500

    Added docstrings to definitions

--- Rita/Lexer/tokenator.h
+++ Rita/Lexer/tokenator.h
@@ -6,17 +6,39 @@
 
 namespace Lexer
 {
-    const Token EOF_TOKEN(TokenType::ENF_OF_FILE);
-
+    const Token EOF_TOKEN = Token(TokenType::END_OF_FILE);
+    
+    /// <summary>
+    /// Class for containing Token-objects.
+    /// </summary>
     class Tokenator
     {
     private:
         std::vector<Token> Tokens;
         size_t currentToken = -1;
     public:
+        /// <summary>
+        /// Checks for next token
+        /// </summary>
+        /// <returns>true or false (depends on result)</returns>
         bool HasNext();
-        Token& Next();
-        Token& Current();
-        Tokenator(std::vector<Token> tokens);
+
+        /// <summary>
+        /// Returns a const reference to next token, EOF_TOKEN will be returned if there is no token.
+        /// </summary>
+        /// <returns>Reference to next token</returns>
+        const Token& Next();
+
+        /// <summary>
+        /// Returns a reference to current token.
+        /// </summary>
+        /// <returns></returns>
+        const Token& Current();
+
+        /// <summary>
+        /// Constructor for Tokenator
+        /// </summary>
+        /// <param name="tokens">Just vector with tokens (will be moved!)</param>
+        Tokenator(std::vector<Token>& tokens);
     };
 }
