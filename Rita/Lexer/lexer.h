57d1d11c36852f9fd16a7a23f36cc9b6f91c7c76
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-06-16 16:19:08 +0500

    Added Tokenize defenition, also added currChar field

--- Rita/Lexer/lexer.h
+++ Rita/Lexer/lexer.h
@@ -4,13 +4,21 @@
 #include <memory>
 #include <string>
 
-#include "token.h"
+#include "tokenator.h"
 
 namespace Lexer 
 {
     class Lexer
     {
-    public:
+    private:
+        size_t currChar = -1;
         
+    public:
+        /// <summary>
+        /// Divide your code into tokens
+        /// </summary>
+        /// <param name="code">An std::string with your code</param>
+        /// <returns>Tokens</returns>
+        Tokenator Tokenize(const std::string& code);
     };
 }
