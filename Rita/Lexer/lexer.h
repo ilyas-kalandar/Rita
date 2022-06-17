edb1f3856f83354326097ef1eb5b1d4e00dec76d
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-06-17 15:38:50 +0500

    Added new fields, Reset and ParseToken definition.

--- Rita/Lexer/lexer.h
+++ Rita/Lexer/lexer.h
@@ -12,13 +12,29 @@ namespace Lexer
     {
     private:
         size_t currChar = -1;
-        
+        size_t currentLine = 0;
+        TokenType currentTokenType;
+
+        std::string& source;
+        std::string currentLexeme;
+
+        std::vector<Token> parsedTokens;
+
+        /// <summary>
+        /// Resets inner fields
+        /// </summary>
+        void Reset();
+
+        /// <summary>
+        /// Parses a token and pushes it to parsedTokens vector.
+        /// </summary>
+        void ParseToken();
     public:
         /// <summary>
         /// Divide your code into tokens
         /// </summary>
         /// <param name="code">An std::string with your code</param>
         /// <returns>Tokens</returns>
-        Tokenator Tokenize(const std::string& code);
+        Tokenator Tokenize(std::string& code);
     };
 }
