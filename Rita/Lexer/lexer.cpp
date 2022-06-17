81638c54b81d039e56fa2045ac7dc94ac113496d
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-06-17 15:39:28 +0500

    Added base for Tokenize implementation, also ParseToken created.

--- Rita/Lexer/lexer.cpp
+++ Rita/Lexer/lexer.cpp
@@ -3,8 +3,42 @@
 
 namespace Lexer
 {
-    Tokenator Lexer::Tokenize(const std::string& code)
+    void Lexer::Reset()
     {
-        //TODO        
+        this->currChar = 0;
+        this->currentLine = 1;
+    }
+
+    void Lexer::ParseToken()
+    {
+        while (true)
+        {
+            switch (this->currentTokenType)
+            {
+            case TokenType::STRING:
+                //TODO
+                break;
+            }
+        }
+    }
+
+    Tokenator Lexer::Tokenize(std::string& code)
+    {
+        this->Reset();
+        this->source = code; // not copy, just reference creation.
+        
+        while (this->currChar < code.size())
+        {
+            // Heart of lexer is here, lets check current charachter
+
+            if (std::isalpha(code[this->currChar]))
+            {
+                /*
+                * If some alha passed, we parse it as an IDENTIFIER
+                */
+                this->currentTokenType = TokenType::IDENTIFIER;
+            }
+            this->ParseToken();
+        }
     }
 }
