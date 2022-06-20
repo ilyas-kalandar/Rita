bc7726f7595de588f58884bb2bbf0bdf6929628d
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-06-20 14:49:34 +0500

    Added PLUS, MINUS, STRINGS and ID's lexing

--- Rita/Lexer/lexer.cpp
+++ Rita/Lexer/lexer.cpp
@@ -33,12 +33,12 @@ namespace Lexer
 
     inline char Lexer::GetNext()
     {
-        if (!HasNext()) 
+        if (!HasNext())
             return '\0';
         return (*sourcePointer)[currChar + 1];
     }
 
-    inline bool Lexer::CheckNext(char ch) 
+    inline bool Lexer::CheckNext(char ch)
     {
         return GetNext() == ch;
     }
@@ -53,7 +53,6 @@ namespace Lexer
 
     void Lexer::ParseToken()
     {
-
         bool working = true;
 
         while (Current() != '\0')
@@ -67,27 +66,44 @@ namespace Lexer
                     working = false;
                     break;
                 case '\n':
-                    //TODO(Ilyas): Throw exception
+                    throw std::runtime_error("Expected '\"', but found '\\n' (" + std::to_string(currentLine) + "," + std::to_string(currChar) + ")");
                     break;
                 }
                 break;
             case TokenType::IDENTIFIER:
-                // TODO 
+                if (Current() == ' ' || Current() == '\n')
+                {
+                    working = false;
+                }
                 break;
-            //TODO(Ilyas): Add another here
+                //TODO(Ilyas): Add another here
+            case TokenType::INTEGER:
+            case TokenType::FLOAT:
+                if (Current() == '.' && currentTokenType == TokenType::FLOAT)
+                {
+                    /*
+                    * If some integer contains dot, parse it as float
+                    */
+                    currentTokenType = TokenType::FLOAT;
+                }
+                else if (!std::isdigit(Current()))
+                {
+                    working = false;
+                }
+                break;
             }
 
             if (!working)
             {
                 /*
-                * Maybe here will be something like 
-                *    
+                * Maybe here will be something like
+                *
                 *       Lexer::Next();
-                * 
+                *
                 */
                 break;
             }
-            
+
             currentLexeme.push_back(Current());
             Next();
         }
@@ -105,16 +121,68 @@ namespace Lexer
         {
             // Heart of lexer is here, lets check current charachter
 
-            if (std::isalpha(code[currChar]))
+            if (std::isalpha(Current()))
             {
                 /*
                 * If some alha passed, we parse it as an IDENTIFIER
                 */
                 currentTokenType = TokenType::IDENTIFIER;
+                ParseToken();
             }
-            ParseToken();
+            else if (std::isdigit(Current()))
+            {
+                /*
+                * If some number passed, parse it as some INTEGER (or FLOAT)
+                *
+                */
+                currentTokenType = TokenType::INTEGER;
+                ParseToken();
+            }
+            else
+            {
+                switch (Current())
+                {
+                case ' ':
+                    break;
+                case '\n':
+                    currentLine++;
+                    break;
+                case '+':
+                    parsedTokens.emplace_back(Token(TokenType::PLUS));
+                    break;
+                case '-':
+                    parsedTokens.emplace_back(Token(TokenType::MINUS));
+                    break;
+                case '/':
+                    parsedTokens.emplace_back(Token(TokenType::DIVISION));
+                    break;
+                case '*':
+                    parsedTokens.emplace_back(Token(TokenType::MULTIPLY));
+                    break;
+                case '\"':
+                    currentTokenType = TokenType::STRING;
+                    Next();
+                    ParseToken();
+                    break;
+                case '=':
+                    // Check for EQUAL_EQUAL
+                    if (CheckNext('='))
+                    {
+                        parsedTokens.emplace_back(Token(TokenType::EQUAL_EQUAL));
+                        Next();
+                    }
+                    else
+                    {
+                        parsedTokens.emplace_back(Token(TokenType::EQUAL));
+                    }
+                    break;
+                default:
+                    std::cout << "WHAT? " << Current() << std::endl;
+                }
+                Next();
+            }
+            
         }
-
         return Tokenator(Lexer::parsedTokens);
     }
 }
