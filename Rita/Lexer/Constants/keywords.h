cabb452021c2c7da90d913761e005df4fe93a4cb
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-06-25 22:10:58 +0500

    Added RESERVED_KEYWORDS constant.

---
+++ Rita/Lexer/Constants/keywords.h
@@ -0,0 +1,10 @@
+namespace Lexer
+{
+    const static std::unordered_map<std::string, TokenType> RESERVED_WORDS =
+    {
+        {"if", TokenType::IF},
+        {"else", TokenType::ELSE},
+        {"fun", TokenType::FUN},
+        {"while", TokenType::WHILE},
+    };
+}
