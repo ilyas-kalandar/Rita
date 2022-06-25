e4e3638151d5a6c83e2e07686e6c519c1a008331
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-06-25 22:10:24 +0500

    Removed RESERVED_WORDS constant

--- Rita/Lexer/lexer.h
+++ Rita/Lexer/lexer.h
@@ -9,13 +9,7 @@
 
 namespace Lexer 
 {
-    const std::unordered_map<std::string, TokenType> RESERVED_WORDS =
-    {
-        {"if", TokenType::IF},
-        {"else", TokenType::ELSE},
-        {"fun", TokenType::FUN},
-        {"while", TokenType::WHILE},
-    };
+
 
     class Lexer
     {
