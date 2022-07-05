6e648d5bbb9fd13b183715d736ade6f3ae2e96fc
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-07-05 21:25:30 +0500

    Added parser definition.

---
+++ Rita/Parser/parser.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "../Lexer/lexer.h"
+
+/**
+ * Ochai mirzo (duokhoni)
+ * 
+ */
+
+class Program
+{
+    //upper env here
+    //vec<instr> here
+    //map<str, RitaObject> here (for variables)
+    //map<str, Program> here (for functions)
+    //map<str, StructureDefinitionInstruction> (for structures)
+    //list<RitaObject*> here (for garbage collection)
+
+    // void CollectGarbage
+    // Start() // starts the program (or sub-program?) 
+
+
+
+};
+
+class Parser
+{
+    Lexer::Tokenator tokens;
+public:
+    Program GetProgram()
+    {
+
+    }
+};  
