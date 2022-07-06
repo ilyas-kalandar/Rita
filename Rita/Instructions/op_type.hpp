f29b530c505e2751b4ccedbd2bccdd1309b592bc
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-07-06 16:55:00 +0500

    Added op_type.hpp

---
+++ Rita/Instructions/op_type.hpp
@@ -0,0 +1,24 @@
+/**
+ * @file op_type.hpp
+ * @author Ilyas Awaitable - t.me/awaitable
+ * @brief File for Operations types.
+ * @version 0.1
+ * @date 2022-07-06
+ * 
+ * @copyright Copyright Awaitable (c) 2022
+ * 
+ */
+
+#ifndef op_type_h
+#define op_type_h
+
+enum class OpType
+{
+    PLUS,
+    MINUS,
+    MUL,
+    DIV,
+    EQUAL_EQUAL,
+};
+
+#endif
