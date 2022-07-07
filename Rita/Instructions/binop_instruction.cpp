43d00e3f073b7c80d6ac601dc81c2debeba4882d
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-07-07 10:50:41 +0500

    Added BinOpInstruction implementation.

---
+++ Rita/Instructions/binop_instruction.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file binon_instruction.cpp
+ * @author Ilyas Awaitable (t.me/awaitable)
+ * @brief Implementations of BinOpInstruction's functions.
+ * @version 0.1
+ * @date 2022-07-07
+ * 
+ * @copyright Copyright Awaitable(c) 2022
+ * 
+ */
+
+#include "binop_instruction.hpp"
+
+BinOpInstruction::BinOpInstruction(std::shared_ptr<Instruction> first, std::shared_ptr<Instruction> second, OpType op_type)
+{
+    this->type = InstructionType::BINOP;
+}
+
+std::shared_ptr<Instruction> BinOpInstruction::GetFirst()
+{
+    return this->first;
+} 
+
+std::shared_ptr<Instruction> BinOpInstruction::GetSecond()
+{
+    return this->second;
+}
+
+OpType BinOpInstruction::GetOperationType()
+{
+    return this->operation_type;
+}
