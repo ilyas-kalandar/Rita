d8b1f2071cbbbfda09c4a8b00558d573dff9bef8
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-07-07 10:26:34 +0500

    Added Leaf definition

---
+++ Rita/Instructions/leaf.hpp
@@ -0,0 +1,41 @@
+/**
+ * @file leaf.hpp
+ * @author Ilyas Awaitable (t.me/awaitable)
+ * @brief Instruction's leaf definition
+ * @version 0.1
+ * @date 2022-07-07
+ * 
+ * @copyright Copyright Awaitable (c) 2022
+ * 
+ */
+
+#include "instruction.hpp"
+#include "../Core/type.h"
+
+#ifndef leaf_hpp
+#define leaf_hpp
+
+/**
+ * @brief Class for storing Objects into instructions.
+ * 
+ */
+class Leaf : public Instruction
+{
+private: 
+    RitaObject* obj;
+public:
+    /**
+     * @brief Construct a new Leaf object
+     * 
+     * @param obj Object for storing
+     */
+    Leaf(RitaObject* obj);
+    /**
+     * @brief Get the stored object
+     * 
+     * @return RitaObject* 
+     */
+    RitaObject* GetObj();
+};
+
+#endif
