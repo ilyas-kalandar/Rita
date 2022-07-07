5a7557c67c5f3899110748161e5b92b4c9bdf4ae
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-07-07 20:57:14 +0500

    Added IfInstruction definition

---
+++ Rita/Instructions/if_instruction.hpp
@@ -0,0 +1,61 @@
+/**
+ * @file if_instruction.hpp
+ * @author Ilyas Awaitable (t.me/awaitable)
+ * @brief Definition of IfInstruction
+ * @version 0.1
+ * @date 2022-07-07
+ * 
+ * @copyright Copyright Awaitable (c) 2022
+ * 
+ */
+
+#include <memory>
+#include <vector>
+
+#include "instruction.hpp"
+
+/**
+ * @brief Class for storing if statements
+ * 
+ * For example:
+ *      if(a == 3) {
+ *          print "hello!";
+ *      }
+ * 
+ *      will be
+ *  
+ *      IfInstruction(
+ *          {
+ *          PrintInstruction("hello"),
+ *          },
+ *          BinOpInstruction("a", "3", EQUAL_EQUAL)
+ *      )
+ * 
+ */
+class IfInstruction : public Instruction
+{
+protected:
+    std::shared_ptr<Instruction> expr;
+    std::vector<std::shared_ptr<Instruction>>& body;
+
+public:
+    /**
+     * @brief Construct a new IfInstruction object
+     * 
+     * @param body vector with instructions
+     * @param expr expression which will return boolean value
+     */
+    IfInstruction(std::vector<std::shared_ptr<Instruction>>& body, std::shared_ptr<Instruction> expr);
+    /**
+     * @brief Get the Expr object
+     * 
+     * @return std::shared_ptr<Instruction> 
+     */
+    std::shared_ptr<Instruction> GetExpr();
+    /**
+     * @brief Get the Body of IfInstruction
+     * 
+     * @return std::vector<std::shared_ptr<Instruction>> 
+     */
+    std::vector<std::shared_ptr<Instruction>> GetBody();
+};
