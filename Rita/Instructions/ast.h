4113537961744f8e07781550cb43a2f27578c470
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-07-07 20:56:55 +0500

    Removed IfInstruction

--- Rita/Instructions/ast.h
+++ Rita/Instructions/ast.h
@@ -10,49 +10,6 @@ enum class InstructionType
 };
 
 
-class AssignmentInstruction : public Instruction
-{
-private:
-    std::string& identifier;
-    std::shared_ptr<Instruction> instr;
-public: 
-    AssignmentInstruction(std::string& id, std::shared_ptr<Instruction> inst) : identifier(id)
-    {
-        this->type = InstructionType::ASSIGNMENT;
-        this->instr = inst;
-    }
-    std::shared_ptr<Instruction> GetRightInstr()
-    {
-        return this->instr;
-    }
-    const std::string& GetIdentifier()
-    {
-        return this->identifier;
-    }
-};
-
-class IfInstruction : public Instruction
-{
-protected:
-    std::shared_ptr<Instruction> expr;
-    std::vector<std::shared_ptr<Instruction>>& body;
-
-public:
-    IfInstruction(std::vector<std::shared_ptr<Instruction>>& body, std::shared_ptr<BinOpInstruction> expr) : body(body)
-    {
-        this->expr = expr;
-        this->type = InstructionType::IF;
-    }
-    std::shared_ptr<BinOpInstruction> GetExpr()
-    {
-        return this->expr;
-    }
-    std::vector<std::shared_ptr<Instruction>> GetBody()
-    {
-        return this->body;
-    }
-};
-
 class WhileInstruction : public IfInstruction
 {
 public:
