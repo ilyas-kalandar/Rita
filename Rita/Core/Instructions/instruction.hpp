8374f61d40547f5da6092d728cd2ba12d89cee9a
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-07-11 18:25:33 +0500

    Added overloading of operator '<<' definition.

--- Rita/Core/Instructions/instruction.hpp
+++ Rita/Core/Instructions/instruction.hpp
@@ -4,31 +4,35 @@
  * @brief Instruction definition.
  * @version 0.1
  * @date 2022-07-07
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
+
+#include <iostream>
 
 #include "instructions_types.hpp"
 
 #ifndef instruction_hpp
 #define instruction_hpp
 
-/**
- * @brief Base class for Interpreter's instruction
- * Don't create instance of this class, use inherited classes instead
- */
+ /**
+  * @brief Base class for Interpreter's instruction
+  * Don't create instance of this class, use inherited classes instead
+  */
 class Instruction
 {
-protected: 
-    InstructionType type;
+protected:
+	InstructionType type;
 public:
-    /**
-     * @brief Get the Type of bject
-     * 
-     * @return InstructionType 
-     */
-    InstructionType GetType();
+	/**
+	 * @brief Get the Type of bject
+	 *
+	 * @return InstructionType
+	 */
+	InstructionType GetType() const;
 };
+
+std::ostream& operator<<(std::ostream& os, const Instruction& instr);
 
 #endif
