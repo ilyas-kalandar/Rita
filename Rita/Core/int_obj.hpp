31a4cb160d4a9643cc6c27aa8394cc921019787f
Ilyas <ilyas_kalandar@outlook.com>
2022-07-14 00:27:51 +0500

    Added definition of Int object.

---
+++ Rita/Core/int_obj.hpp
@@ -0,0 +1,32 @@
+#include "object.hpp"
+#include "type.hpp"
+
+#include "string_obj.hpp"
+#include "native_function.hpp"
+
+#ifndef int_obj_hpp
+#define int_obj_hpp
+
+class Int : public RitaObject
+{
+protected:
+	int value;
+public:
+	/**
+	* @brief Constructs new int object
+	*
+	* @param val A value for storing
+	* @param objectType An object's type.
+	*/
+	Int(int val, Type* objectType);
+
+	/**
+	* @brief Get value of object
+	*
+	* @return Value of obj
+	*
+	*/
+	int GetValue();
+};
+
+#endif
