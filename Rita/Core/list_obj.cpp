5ff1ae96319b917f520aee3d679d1b0676bd01ed
Ilyas <ilyas_kalandar@outlook.com>
2022-07-14 12:56:16 +0500

    List moved to Core, also docs added.

--- Rita/Core/list_obj.cpp
+++ Rita/Core/list_obj.cpp
@@ -1,26 +1,40 @@
+/**
+ * @file list_obj.cpp
+ * @author Ilyas Awaitable (t.me/awaitable)
+ * @brief List-object implementation.
+ * @version 0.1
+ * @date 2022-07-14
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
 #include "list_obj.hpp"
 
-List::List(Type* type)
+namespace Core
 {
-    this->objectType = type;
-}
+    List::List(Type* type)
+    {
+        this->objectType = type;
+    }
 
-void List::Insert(size_t index, RitaObject* obj)
-{
-    this->vec.insert(vec.begin() + index, obj);
-}
+    void List::Insert(size_t index, RitaObject* obj)
+    {
+        this->vec.insert(vec.begin() + index, obj);
+    }
 
-void List::Pop()
-{
-    this->vec.pop_back();
-}
+    void List::Pop()
+    {
+        this->vec.pop_back();
+    }
 
-void List::Push(RitaObject* obj)
-{
-    this->vec.push_back(obj);
-}
+    void List::Push(RitaObject* obj)
+    {
+        this->vec.push_back(obj);
+    }
 
-void List::PushFront(RitaObject* obj)
-{
-    this->vec.insert(vec.begin(), obj);
+    void List::PushFront(RitaObject* obj)
+    {
+        this->vec.insert(vec.begin(), obj);
+    }
 }
