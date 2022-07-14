101682502db14a28fee1f204950ebab73276484a
Ilyas <ilyas_kalandar@outlook.com>
2022-07-14 12:56:16 +0500

    List moved to Core, also docs added.

--- Rita/Core/list_obj.hpp
+++ Rita/Core/list_obj.hpp
@@ -1,11 +1,11 @@
 /**
  * @file list_obj.hpp
- * @author your name (you@domain.com)
- * @brief 
+ * @author Ilyas Awaitable (t.me/awaitable)
+ * @brief List-object definition.
  * @version 0.1
  * @date 2022-07-13
  * 
- * @copyright Copyright (c) 2022
+ * @copyright Copyright Awaitable (c) 2022
  * 
  */
 
@@ -16,18 +16,57 @@
 #ifndef list_obj_hpp
 #define list_obj_hpp
 
-class List : RitaObject
+namespace Core
 {
-protected:
-    std::vector<RitaObject*> vec;
-public:
-    List(Type* type);
-    
-    void Push(RitaObject*);
-    void PushFront(RitaObject*);
-    void Insert(size_t index, RitaObject*);
-    void Pop();
-    void PopFront();
-};
+    /**
+     * @brief Class-container for std::vector<RitaObject*> for using directly from Rita
+     * 
+     */
+    class List : RitaObject
+    {
+    protected:
+        std::vector<RitaObject*> vec;
+    public:
+        /**
+         * @brief Construct a new List object
+         * 
+         * @param type Type of object
+         */
+        List(Type* type);
+        
+        /**
+         * @brief Adds object to end of list
+         * 
+         * @param obj Object for adding
+         */
+        void Push(RitaObject* obj);
+
+        /**
+         * @brief Adds an object to begining of list
+         * @param obj Object for adding
+         */
+        void PushFront(RitaObject* obj);
+
+        /**
+         * @brief Inserts an object before ``index``
+         * 
+         * @param index Index
+         * @param obj Object for adding
+         */
+        void Insert(size_t index, RitaObject* obj);
+        
+        /**
+         * @brief Removes last object of list
+         * 
+         */
+        void Pop();
+
+        /**
+         * @brief Removes first object of list
+         * 
+         */
+        void PopFront();
+    };
+}
 
 #endif
