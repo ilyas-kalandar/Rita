463317b976b14f3921a56f5a02ded0468d60804e
Ilyas <ilyas_kalandar@outlook.com>
2022-07-15 14:03:01 +0500

    Moved IfInstruction to Core::Instructions

--- Rita/Core/Instructions/if_instruction.cpp
+++ Rita/Core/Instructions/if_instruction.cpp
@@ -11,17 +11,23 @@
 
 #include "if_instruction.hpp"
 
-IfInstruction::IfInstruction(std::vector<std::shared_ptr<Instruction>>& body, std::shared_ptr<Instruction> expr) : body(body)
+namespace Core
 {
-    this->expr = expr;
-    this->type = InstructionType::IF;
-}
+    namespace Instructions
+    {
+        IfInstruction::IfInstruction(std::vector<std::shared_ptr<Instruction>>& body, std::shared_ptr<Instruction> expr) : body(body)
+        {
+            this->expr = expr;
+            this->type = InstructionType::IF;
+        }
 
-std::shared_ptr<Instruction> IfInstruction::GetExpr()
-{
-    return this->expr;
-}
-std::vector<std::shared_ptr<Instruction>> IfInstruction::GetBody()
-{
-    return this->body;
+        std::shared_ptr<Instruction> IfInstruction::GetExpr()
+        {
+            return this->expr;
+        }
+        std::vector<std::shared_ptr<Instruction>> IfInstruction::GetBody()
+        {
+            return this->body;
+        }
+    }
 }
