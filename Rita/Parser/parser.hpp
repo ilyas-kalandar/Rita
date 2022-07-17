9c1c466b00ac4abe0755f170a7bd2a4e37251e2c
Ilyas <ilyas_kalandar@outlook.com>
2022-07-17 10:38:45 +0500

    Added definiton of base-methods.

--- Rita/Parser/parser.hpp
+++ Rita/Parser/parser.hpp
@@ -1,79 +1,30 @@
 /**
  * @file parser.hpp
+ *
  * @author your name (you@domain.com)
- * @brief
+ * @brief 
  * @version 0.1
- * @date 2022-07-11
- *
+ * @date 2022-07-14
+ * 
  * @copyright Copyright (c) 2022
- *
+ * 
  */
 
 #include <vector>
-#include <memory>
 
+#include "tokenator.hpp"
 #include "Instructions/instruction.hpp"
-#include "Instructions/leaf.hpp"
-#include "Instructions/if_instruction.hpp"
 
-#include "lexer.hpp"
-
-#ifndef parser_hpp
-#define parser_hpp
-
-namespace Parser
+/**
+ * @brief 
+ * 
+ */
+class Parser
 {
-	class Parser
-	{
-	private:
-		InstructionType currentInstructionType;
-		Lexer::Tokenator tok;
-		std::vector<std::shared_ptr<Instruction>> instructions;
-		std::shared_ptr<Instruction> currentInstruction;
-
-		void HandleIdentifier()
-		{
-			auto nextToken = tok.Next();
-			switch (nextToken.GetTokenType())
-			{
-			case Lexer::TokenType::EQUAL:
-				currentInstruction = new IfInstruction();
-			default:
-				throw std::runtime_error("Unexpected token \"" + nextToken.GetLiteral() + "\"");
-			}
-		}
-	public:
-		std::vector<std::shared_ptr<Instruction>> Parse()
-		{
-			while (tok.Current().GetTokenType() != Lexer::TokenType::END_OF_FILE)
-			{
-				switch (tok.Current().GetTokenType())
-				{
-				case Lexer::TokenType::IDENTIFIER:
-					/**
-					* If identifier give, instruction may be:
-					*	FunctionCall
-					*	AssignInstruction
-					*	BinaryOperation
-					*/
-
-					// handle
-					this->HandleIdentifier();
-					break;
-				case Lexer::TokenType::FUN:
-					// It is function-definition, all is ok.
-					break;
-				case Lexer::TokenType::IF:
-					// if, block
-					break;
-				case Lexer::TokenType::WHILE:
-					// absolutely while, it is ok
-					break;
-				}
-			}
-			return std::vector<std::shared_ptr<Instruction>>();
-		}
-	};
-}
-
-#endif
+protected:
+    Lexer::Tokenator tokens;
+public:
+    std::vector<std::shared_ptr<Core::Instructions::Instruction>> Parse(Lexer::Tokenator& tokens);
+    std::shared_ptr<Core::Instructions::Instruction> ParseByPriority(size_t);
+    std::shared_ptr<Core::Instructions::Instruction> ParseExpression(Lexer::Tokenator& tok);
+};
