e780b4671f0457ecc89298b4e3eadd8989e7ae01
Ilyas <ilyas_kalandar@outlook.com>
2022-07-19 14:43:06 +0500

    Fixed problem with std::shared_ptr, also docs added.

--- Rita/Core/Instructions/unop_instruction.hpp
+++ Rita/Core/Instructions/unop_instruction.hpp
@@ -1,3 +1,16 @@
+/**
+ * @file unop_instruction.hpp
+ * @author Ilyas Awaitable (t.me/awaitable)
+ * @brief UnaryOperatorInstruction definition
+ * @version 0.1
+ * @date 2022-07-18
+ * 
+ * @copyright Copyright Awaitable (c) 2022
+ * 
+ */
+
+#include <memory>
+
 #include "instruction.hpp"
 #include "op_type.hpp"
 
@@ -8,6 +21,12 @@ namespace Core
 {
 	namespace Instructions
 	{
+		/**
+		 * @brief Class for storing instruction like
+		 * 
+		 * "- someObject"
+		 * 
+		 */
 		class UnaryOperatorInstruction : public Instruction
 		{
 		protected:
