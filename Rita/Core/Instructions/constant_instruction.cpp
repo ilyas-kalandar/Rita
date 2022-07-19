ef1ed8f36b709897a0c57516dfb679ed76d902f7
Ilyas <ilyas_kalandar@outlook.com>
2022-07-20 00:48:37 +0500

    Added ConstantInstruction.

---
+++ Rita/Core/Instructions/constant_instruction.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file constant_instruction.cpp
+ * @author Ilyas Awaitable (t.me/awaitable)
+ * @brief ConstantInstruction's implementation
+ * @version 0.1
+ * @date 2022-07-20
+ * 
+ * @copyright Copyright Awaitable (c) 2022
+ * 
+ */
+
+#include "instructions_types.hpp"
+#include "constant_instruction.hpp"
+
+namespace Core
+{
+    namespace Instructions
+    {
+        template<class T>
+        ConstantInstruction<T>::ConstantInstruction(T data)
+        {
+            this->data = data;
+            this->type = InstructionType::CONSTANT;
+        }
+
+        template<class T>
+        T ConstantInstruction<T>::GetData()
+        {
+            return this->data;
+        }
+    }
+}
