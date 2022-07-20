c57fbd3e175680389e9a6fbf09a148e6a5f6787e
Ilyas <ilyas_kalandar@outlook.com>
2022-07-20 20:27:28 +0500

    Added constants_types.hpp

---
+++ Rita/Core/Instructions/constants_types.hpp
@@ -0,0 +1,23 @@
+/**
+ * @file constants_types.hpp
+ * @author Ilyas Awaitable (t.me/awaitable)
+ * @brief Definition of ConstantType
+ * @version 0.1
+ * @date 2022-07-20
+ * 
+ * @copyright Copyright Awaitable (c) 2022
+ * 
+ */
+
+#ifndef constants_types_hpp
+#define constants_types_hpp
+
+enum class ConstantType
+{
+    INT, 
+    STRING,
+    FLOAT,
+    LIST,
+};
+
+#endif
