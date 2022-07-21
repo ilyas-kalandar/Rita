ce274857d67689e01486a980d07c803d17d621b7
Ilyas <ilyas_kalandar@outlook.com>
2022-07-21 17:15:51 +0500

    Added var_decl_instruction.cpp & VariableDeclarationInstruction implementation

---
+++ Rita/Core/Instructions/var_decl_instruction.cpp
@@ -0,0 +1,27 @@
+/**
+ * @file var_decl_instruction.cpp
+ * @author Ilyas Awaitable (t.me/awaitable)
+ * @brief VariableDeclarationInstruction implementation.
+ * @version 0.1
+ * @date 2022-07-21
+ * 
+ * @copyright Copyright Awaitable (c) 2022
+ * 
+ */
+
+#include "var_decl_instruction.hpp"
+
+namespace Core
+{
+    namespace Instructions
+    {
+        VariableDeclarationInstruction::VariableDeclarationInstruction(std::string& varName, std::shared_ptr<Instruction> instr)
+            :
+            varName(varName),
+            expr(instr)
+
+        {
+            this->type = InstructionType::VAR_DECL;
+        }
+    }
+}
