4e44e0fd00d41bf0af96a2b1c16632a5c758d8e7
Ilyas <ilyas_kalandar@outlook.com>
2022-07-26 00:49:41 +0500

    Added op_type.cpp

---
+++ Rita/Core/Instructions/op_type.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+#include "op_type.hpp"
+
+namespace Core
+{
+    namespace Instructions
+    {
+        std::ostream& operator<<(std::ostream& os, OpType op)
+        {
+            switch(op)
+            {
+            case OpType::AND:
+                os << "And &&";
+                break;
+            case OpType::DIV:
+                os << "Divide /";
+                break;
+            case OpType::LESS_OR_EQUAL_THAN:
+                os << "Less or equal <=";
+                break;
+            case OpType::EQUAL_EQUAL:
+                os << "Equal-equal ==";
+                break;
+            case OpType::PLUS:
+                os << "Plus +";
+                break;
+            case OpType::MUL:
+                os << "Multiply *";
+                break;
+            case OpType::NOT:
+                os << "Not !";
+                break;
+            case OpType::GREATER_OR_EQUAL_THAN:
+                os << "Greater or equal >=";
+                break;
+            case OpType::MINUS:
+                os << "Minus -";
+                break;
+            }
+            return os;
+        }
+    }
+}
