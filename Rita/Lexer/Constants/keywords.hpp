55800326e743f73e6be585d36b339be66e3b5c9e
Ilyas <ilyas_kalandar@outlook.com>
2022-07-27 00:15:56 +0500

    Removed 'print'-keyword.

--- Rita/Lexer/Constants/keywords.hpp
+++ Rita/Lexer/Constants/keywords.hpp
@@ -6,7 +6,6 @@ namespace Lexer
         {"else", TokenType::ELSE},
         {"fun", TokenType::FUN},
         {"while", TokenType::WHILE},
-        {"print", TokenType::PRINT},
         {"var", TokenType::VAR},
         {"or", TokenType::OR}, 
         {"and", TokenType::AND},
