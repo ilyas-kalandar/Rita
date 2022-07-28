aee57e0aa5a384a726f499f2f1ec30f6eeca85ee
Ilyas <ilyas_kalandar@outlook.com>
2022-07-28 20:13:42 +0500

    Fixed docstrings.

--- Rita/Core/Instructions/function_definition.hpp
+++ Rita/Core/Instructions/function_definition.hpp
@@ -1,11 +1,11 @@
 /**
  * @file function_definition.hpp
- * @author your name (you@domain.com)
- * @brief 
+ * @author Ilyas Awaitable (t.me/awaitable)
+ * @brief FunctionDefinitionInstruction definition
  * @version 0.1
  * @date 2022-07-28
  * 
- * @copyright Copyright (c) 2022
+ * @copyright Copyright Awaitable (c) 2022
  * 
  */
 
