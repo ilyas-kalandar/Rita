218ecf309ef5141999f07fc9bc7d1406a82a54b5
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-08-09 11:43:31 +0500

    Added enviropment_type.hpp

---
+++ Rita/Executor/enviropment_type.hpp
@@ -0,0 +1,24 @@
+/**
+ * @file enviropment_type.hpp
+ * @author Ilyas Awaitable (t.me/awaitable)
+ * @brief Definition of EnviropmentType
+ * @version 0.1
+ * @date 2022-08-09
+ * 
+ * @copyright Copyright Awaitable (Ilyas Qalandarzoda) (c) 2022
+ * 
+ */
+
+#ifndef enviropment_type_hpp
+#define enviropment_type_hpp
+
+namespace Executor
+{
+    enum class EnviropmentType
+    {
+        FUNCTION,
+        OTHER,
+    };
+}
+
+#endif
