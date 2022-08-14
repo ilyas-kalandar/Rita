ed323ae0ba95d413c22c02f89addd467de109c46
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-08-14 13:52:25 +0500

    Using new Interpreter class for running programs.

--- Rita/Interpreter/main.cpp
+++ Rita/Interpreter/main.cpp
@@ -12,52 +12,11 @@
 #include "ast_printer.hpp"
 #include "string_obj.hpp"
 
-int main()
+#include "interpreter.hpp"
+
+int main(int argc, const char* argv[])
 {
-    size_t line = 1;
+    Interpreter::Interpreter i(argv, argc);
 
-    Lexer::Lexer lex;
-    std::string source;
-    std::string buff;
-
-
-    Executor::Engine engine;
-    
-
-    do
-    {
-        std::cout << "[Line " << line << "]: ";
-        std::getline(std::cin, buff);
-        source += buff;
-        line++;
-    } while (buff.size() > 0);
-
-    auto toks = lex.Tokenize(source);
-    while (toks.Current().GetTokenType() != Lexer::TokenType::END_OF_FILE)
-    {
-        //std::cout << toks.Current() << std::endl;
-        toks.Next();
-    }
-
-    toks.Reset();
-
-    Parser::Parser parser;
-
-    std::shared_ptr<Core::Instructions::Instruction> program;
-
-    try
-    {
-        program = parser.Parse(toks);
-    }
-    catch(const std::runtime_error& e)
-    {
-        std::cout << e.what() << std::endl;
-        return -1; // compilation error
-    }
-
-    Utils::AstPrinter printer(2);
-
-    printer.Print(program.get());
-
-    engine.ExecuteInstruction(program);
+    i.Run();
 }
