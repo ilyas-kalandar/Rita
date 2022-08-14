793524c2dc4b6ed41de1befa6067b6916ea00d7d
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-08-14 13:51:03 +0500

    Added sqrt and needed operators.

--- Rita/Executor/Builtins/functions.cpp
+++ Rita/Executor/Builtins/functions.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <sstream>
 
 #include "rita_exception.hpp"
@@ -90,6 +91,8 @@ namespace Executor
                 }
 
                 std::cout << std::endl;
+
+                return nullptr;
             }
 
             Core::RitaObject* BoolCtor(const std::vector<Core::RitaObject*>& vec)
@@ -126,6 +129,141 @@ namespace Executor
                 }
 
                 return new Core::String(_fl, Types::StringType);
+            }
+
+            Core::RitaObject* IntOperatorEqual(const std::vector<Core::RitaObject*>& list)
+            {
+                auto self = static_cast<Core::Int*>(list[0]);
+                auto second = list[1];
+
+                auto secType = static_cast<Core::Type*>(second->GetType());
+
+                if(secType->GetTypeName() == Types::IntType->GetTypeName())
+                {
+                    return new Core::BoolObject(self->GetValue() == static_cast<Core::Int*>(second)->GetValue(), Types::BoolType);
+                }
+
+                throw Utils::RitaException(
+                    "Executor", 
+                    "Runtime error, expected 'int' or 'float' type, got '" + secType->GetTypeName() + "'"
+                );
+            }
+
+            Core::RitaObject* IntOperatorLessThan(const std::vector<Core::RitaObject*>& list)
+            {
+                auto self = static_cast<Core::Int*>(list[0]);
+                auto second = list[1];
+
+                auto secType = static_cast<Core::Type*>(second->GetType());
+
+                if(secType->GetTypeName() == Types::IntType->GetTypeName())
+                {
+                    return new Core::BoolObject(self->GetValue() < static_cast<Core::Int*>(second)->GetValue(), Types::BoolType);
+                }
+
+                throw Utils::RitaException(
+                    "Executor", 
+                    "Runtime error, expected 'int' or 'float' type, got '" + secType->GetTypeName() + "'"
+                );
+            }
+
+            Core::RitaObject* SqrtNative(const std::vector<Core::RitaObject*>& list)
+            {
+                auto obj = list[0];
+                
+                if(static_cast<Core::Type*>(obj->GetType())->GetTypeName() != "Int")
+                {
+                    throw Utils::RitaException(
+                        "Print", 
+                        "Expected string, got \"" + static_cast<Core::Type*>(obj->GetType())->GetTypeName() + "\""
+                    );
+                }
+                return new Core::Int(std::sqrt(static_cast<Core::Int*>(obj)->GetValue()), Types::IntType);
+            
+            }
+
+            Core::RitaObject* InputNative(const std::vector<Core::RitaObject*>& list)
+            {
+                std::string buff;
+
+                std::getline(std::cin, buff);
+
+                return new Core::String(buff, Types::StringType);
+            }
+
+            Core::RitaObject* IntCtor(const std::vector<Core::RitaObject*>& vec)
+            {
+                auto arg = vec[0];
+
+                auto type = arg->GetType();
+
+                if(type == Builtins::Types::StringType)
+                {
+                    Core::String* tmpStr = static_cast<Core::String*>(arg);
+                    std::cout << "IntCtor, tmpStr value " << tmpStr->GetValue() << std::endl;
+                    return new Core::Int(std::stoi(tmpStr->GetValue()), Builtins::Types::IntType);
+                }
+                else 
+                {
+                    throw Utils::RitaException(
+                        "Int::Constructor",
+                        "Expected int, string, float or bool, given \"" + static_cast<Core::Type*>(arg->GetType())->GetTypeName() + "\""
+                    );
+                }
+            }
+
+            Core::RitaObject* IntOperatorDiv(const std::vector<Core::RitaObject*>& list)
+            {
+                auto self = static_cast<Core::Int*>(list[0]);
+                auto second = list[1];
+
+                auto secType = static_cast<Core::Type*>(second->GetType());
+
+                if(secType->GetTypeName() == Types::IntType->GetTypeName())
+                {
+                    return new Core::Int(self->GetValue() / static_cast<Core::Int*>(second)->GetValue(), Types::IntType);
+                }
+
+                throw Utils::RitaException(
+                    "Executor", 
+                    "Runtime error, expected 'int' or 'float' type, got '" + secType->GetTypeName() + "'"
+                );
+            }
+
+            Core::RitaObject* IntOperatorMul(const std::vector<Core::RitaObject*>& list)
+            {
+                auto self = static_cast<Core::Int*>(list[0]);
+                auto second = list[1];
+
+                auto secType = static_cast<Core::Type*>(second->GetType());
+
+                if(secType->GetTypeName() == Types::IntType->GetTypeName())
+                {
+                    return new Core::Int(self->GetValue() * static_cast<Core::Int*>(second)->GetValue(), Types::IntType);
+                }
+
+                throw Utils::RitaException(
+                    "Executor", 
+                    "Runtime error, expected 'int' or 'float' type, got '" + secType->GetTypeName() + "'"
+                );
+            }
+
+            Core::RitaObject* IntOperatorMinus(const std::vector<Core::RitaObject*>& list)
+            {
+                auto self = static_cast<Core::Int*>(list[0]);
+                auto second = list[1];
+
+                auto secType = static_cast<Core::Type*>(second->GetType());
+
+                if(secType->GetTypeName() == Types::IntType->GetTypeName())
+                {
+                    return new Core::Int(self->GetValue() - static_cast<Core::Int*>(second)->GetValue(), Types::IntType);
+                }
+
+                throw Utils::RitaException(
+                    "Executor", 
+                    "Runtime error, expected 'int' or 'float' type, got '" + secType->GetTypeName() + "'"
+                );
             }
         }
     }
